4ced72d4756aa078cd13f9c1cecd6a7717156782
Petarda-Atomica <perfect.minecraft.andrei@gmail.com>
2024-06-24 02:10:49 +0300

    Added a sidebar to the project browser

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"image"
 	"image/color"
 	"log"
+	"math"
 	"os"
 	"path"
 
@@ -16,6 +17,7 @@ import (
 	"github.com/gopxl/pixel/pixelgl"
 	"github.com/gopxl/pixel/text"
 	"github.com/sqweek/dialog"
+	"golang.org/x/image/colornames"
 	"golang.org/x/image/font/basicfont"
 )
 
@@ -23,24 +25,31 @@ var sceneName string = "Home Page"
 var openProject modDirectory
 var projectLocation string
 
+var win1Visible = false
+var win2Visible = false
+
 var win1 *pixelgl.Window
 var win2 *pixelgl.Window
 var folderpic pixel.Picture
+var closedfolderpic pixel.Picture
 var newfilepic pixel.Picture
+var cubepic pixel.Picture
 var atlas *text.Atlas
 
 var WD string
 
 type mod struct {
 	Name        string `json:"name"`
+	URL         string `json:"presentation"`
 	DownloadURL string `json:"link"`
 	Version     string `json:"version"`
 }
 
 type modDirectory struct {
-	Name    string         `json:"name"`
-	Mods    []mod          `json:"mods"`
-	Subdirs []modDirectory `json:"folders"`
+	Name      string         `json:"name"`
+	Mods      []mod          `json:"mods"`
+	Subdirs   []modDirectory `json:"folders"`
+	collapsed bool
 }
 
 func RGBA(r uint8, g uint8, b uint8, a uint8) color.Color {
@@ -81,7 +90,15 @@ func run() {
 	if err != nil {
 		panic(err)
 	}
+	closedfolderpic, err = loadPicture(path.Join(WD, "/icons/folder-closed.png"))
+	if err != nil {
+		panic(err)
+	}
 	newfilepic, err = loadPicture(path.Join(WD, "/icons/new-document.png"))
+	if err != nil {
+		panic(err)
+	}
+	cubepic, err = loadPicture(path.Join(WD, "/icons/block.png"))
 	if err != nil {
 		panic(err)
 	}
@@ -92,6 +109,7 @@ func run() {
 		Bounds:    pixel.R(0, 0, 1024, 768),
 		Resizable: true,
 		VSync:     true,
+		Invisible: true,
 	}
 	win1, err = pixelgl.NewWindow(cfg)
 	if err != nil {
@@ -106,6 +124,7 @@ func run() {
 		Bounds:    pixel.R(0, 0, 1024, 768),
 		Resizable: false,
 		VSync:     true,
+		Invisible: true,
 	}
 	win2, err = pixelgl.NewWindow(cfg)
 	if err != nil {
@@ -116,19 +135,155 @@ func run() {
 		switch sceneName {
 		case "Home Page":
 			homePage()
+		case "Project Browser":
+			projectBrowser()
 		}
-		win2.Update()
-		win1.Update()
+		if win2Visible {
+			win2.Update()
+		}
+		if win1Visible {
+			win1.Update()
+		}
 	}
+}
+
+func drawSidebar() mod {
+	// Variables
+	screen := win1.Bounds()
+	var pressedMod mod
+
+	// Render folders toolbar
+	leftBar := imdraw.New(nil)
+	leftBar.Color = RGBA(1, 0, 20, 255)
+	leftBar.Push(pixel.V(0, 0), pixel.V(0, screen.H()), pixel.V(300, screen.H()), pixel.V(300, 0))
+	leftBar.Polygon(0)
+	leftBar.Draw(win1)
+
+	// Render folders
+	var drawIndex int
+	var objectHeight float64
+	for _, val := range openProject.Subdirs {
+		// Draw folder
+		var sprite *pixel.Sprite
+		if val.collapsed {
+			sprite = pixel.NewSprite(folderpic, folderpic.Bounds())
+		} else {
+			sprite = pixel.NewSprite(closedfolderpic, closedfolderpic.Bounds())
+		}
+		objectHeight = drawSidebarItem(val.Name, sprite, screen, drawIndex, sidebarScrollIndex)
+		drawIndex++
+
+		// Check if collapsed
+		if !val.collapsed {
+			continue
+		}
+
+		// Render subfiles
+		for _, vaj := range val.Mods {
+			sprite = pixel.NewSprite(cubepic, cubepic.Bounds())
+			drawSidebarItem(vaj.Name, sprite, screen, drawIndex, sidebarScrollIndex)
+			drawIndex++
+		}
+
+	}
+
+	// Render non-foldered mods
+	for _, val := range openProject.Mods {
+		sprite := pixel.NewSprite(cubepic, cubepic.Bounds())
+		drawSidebarItem(val.Name, sprite, screen, drawIndex, sidebarScrollIndex)
+		drawIndex++
+	}
+
+	// Handle sidebar scrolling
+	if win1.MousePosition().X <= 200 {
+		scrollingPower := 7.5
+		sidebarScrollIndex += win1.MouseScroll().Y * scrollingPower
+
+		if screen.H()-(float64(drawIndex+1)*objectHeight-sidebarScrollIndex) > 0 {
+			sidebarScrollIndex = float64(drawIndex+1)*objectHeight - screen.H()
+		}
+
+		if sidebarScrollIndex < 0 || float64(drawIndex+1)*objectHeight < screen.H() {
+			sidebarScrollIndex = 0
+		}
+
+	}
+
+	// Handle clicking
+	if win1.MousePosition().X < 200 && win1.JustPressed(pixelgl.MouseButtonLeft) {
+		objectNO := int(math.Floor((screen.H() - win1.MousePosition().Y + sidebarScrollIndex) / objectHeight))
+
+		// Determine what was clicked and take action
+		func() {
+			for i, val := range openProject.Subdirs {
+				if objectNO == 0 {
+					openProject.Subdirs[i].collapsed = !openProject.Subdirs[i].collapsed
+					return
+				}
+
+				if val.collapsed {
+					if len(val.Mods) < objectNO {
+						objectNO -= len(val.Mods)
+					} else {
+						pressed := val.Mods[objectNO-1]
+						pressedMod = pressed
+						return
+					}
+				}
+
+				objectNO--
+			}
+
+			if objectNO >= len(openProject.Mods) {
+				return
+			}
+
+			pressed := openProject.Mods[objectNO]
+			pressedMod = pressed
+		}()
+	}
+
+	return pressedMod
+}
+
+func drawSidebarItem(name string, sprite *pixel.Sprite, screen pixel.Rect, drawIndex int, scrollIndex float64) float64 {
+	// Load icon
+	marginTop := 10.0
+	marginLeft := 10.0
+	spriteScale := 0.07
+
+	// Draw icon
+	sprite.Draw(win1, pixel.IM.Scaled(pixel.V(0, 0), spriteScale).Moved(pixel.V(sprite.Frame().W()*spriteScale/2+marginLeft, scrollIndex+screen.H()-(sprite.Frame().H()*spriteScale+marginTop)*float64(drawIndex)-marginTop-sprite.Frame().H()*spriteScale/2)))
+
+	// Draw folder title
+	txt := text.New(pixel.V(0, 0), atlas)
+	txt.Color = colornames.White
+	for i, val := range name {
+		if i > 13 {
+			fmt.Fprint(txt, "...")
+			break
+		}
+		fmt.Fprint(txt, string(val))
+	}
+	textScale := .7
+	textScale *= sprite.Frame().H() * spriteScale / txt.Bounds().H()
+	txt.Draw(win1, pixel.IM.Scaled(pixel.V(0, 0), textScale).Moved(pixel.V(2*marginLeft+sprite.Frame().W()*spriteScale, scrollIndex+screen.H()-float64(drawIndex+1)*(marginTop+sprite.Frame().H()*spriteScale))))
+
+	return marginTop + sprite.Frame().H()*spriteScale
 }
 
 var openedFileSelectDialog = false
 
 func homePage() {
-	// Variables
-
 	// Set windows
-	win1.Hide()
+	if win1Visible {
+		win1.Hide()
+		win1Visible = false
+	}
+	if !win2Visible {
+		win2.Show()
+		win2Visible = true
+	}
 	win2.SetBounds(pixel.R(0, 0, 600, 400))
 
 	// Update
@@ -234,6 +389,28 @@ func homePage() {
 				sceneName = "Project Browser"
 			}()
 		}
+	}
+}
+
+var sidebarScrollIndex float64
+
+func projectBrowser() {
+	// Manage windows
+	if !win1Visible {
+		win1.Show()
+		win1Visible = true
+	}
+	if win2Visible {
+		win2.Hide()
+		win2Visible = false
+	}
+
+	// Update screen
+	win1.Clear(RGBA(6, 24, 38, 255))
+
+	press := drawSidebar()
+	if press.Name != "" {
+		log.Println("Pressed mod", press.Name)
 	}
 }
 
